8a7639ce17a7ceb4c84fb47796ef6c2c90a84c15
Vladyslav Bardin <vbardin810@gmail.com>
2024-11-09 14:39:46 +0200

    Add an endpoint to receive and handle mentoring request

--- backend/Portfolio.Api/Program.cs
+++ backend/Portfolio.Api/Program.cs
@@ -1,3 +1,4 @@
+using Portfolio.Api.Configuration;
 using Portfolio.Api.Endpoints;
 using Portfolio.Api.Extensions.DependencyInjection;
 using Portfolio.Api.Workspaces.Services;
@@ -8,8 +9,15 @@ builder.Configuration
     .AddUserSecrets<Program>()
     .AddEnvironmentVariables();
 
+builder.Services.AddCors(corsOptions =>
+    corsOptions.AddPolicy("CorsPolicy",
+        policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ContactInfoOptions>(
+    builder.Configuration.GetSection(ContactInfoOptions.SectionName));
 
 builder.Services.AddFluentEmailServices();
 builder.Services.AddGoogleServices(builder.Configuration);
@@ -23,6 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 app.MapHealthEndpoints();
 app.MapPortfolioEndpoints();
 
